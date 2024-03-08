a928f25c91d0554dd0bbd5825c0cc5067f362e23
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-07 17:38:21 -0800

    Q 13 of Ts/js & node js of 45 assignment
    Q 13 of Ts/js & node js of 45 assignment

---
+++ main.js
@@ -0,0 +1,2 @@
+var vehicles = ["Honda Motorcycle", "Audi", "Prado", "Hilux Revo"];
+vehicles.map(function (items) { return console.log("I would like to own a ".concat(items)); });
---
+++ main.ts
@@ -0,0 +1,4 @@
+let vehicles : string[] = ["Honda Motorcycle","Audi","Prado","Hilux Revo"];
+
+
+vehicles.map((items) => console.log(`I would like to own a ${items}`));
